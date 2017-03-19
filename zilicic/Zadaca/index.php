52495b761605b4e355177431cbc28ecb7bd74f8a
zilicic <zilicic5@h-1.hr>
2017-03-19 22:36:03 +0100

    Zadaca

--- zilicic/Zadaca/index.php
+++ zilicic/Zadaca/index.php
@@ -1,39 +1,27 @@
-
-<html>
-<head>
-    <title> Domaći uradak</title>
-</head>
-<body>
-    <H2  style="color:red;" align="center">Kreiranja stranice - vježba</H2> <br>
-    
-    <h3><u><i>Zaduženja do slijedećeg predavanja</i></u> </h3>
-<ol type="1">
-<li>Instalacija Netbeans</li>
-<li>Instalacija XAMPP</li>
-<li>Pogledati što je FTP</li>
-<li>Pogledati primjere HTML-a na :&nbsp;&nbsp;<a href=" https://www.w3schools.com" >HTML</a></li>
-</ol><br>
-<h3><u><i>Status zaduženja</i></u> </h3>
- <dl>
-  <dt>ad1</dt>
-  <dd>- Instaliran i pokrenut</dd>
-  <dt>ad2</dt>
-  <dd>- Instaliran i pomjenjene postavke apache servera</dd>
-   <dt>ad3</dt>
-   <dd>- FTP ( File Transfer Protocol ) <br>
-       je Internet protokol koji omogućava razmjenu (prijenos) datoteka između dva računala preko TCP/IP mreže. FTP radi na principu client/server tehnologije. Podaci su postavljeni na FTP servere s kojima komuniciraju FTP clienti preko odgovarajućih naredbi za komunikaciju pomoću kojih se omogućuje spajanje na FTP server, pregledavanje i traženje potrebnih datoteka te njihovo skidanje odnosno postavljanje. Korisniku nije potrebno poznavanje naredbi za korištenje FTP servisa, nego samo poznavanje rada sa FTP clientima koji to za njih rade......<br>
-       <a href="https://www.carnet.hr/referalni/obrazovni/imme/ftp1.html">Tekst je preuzet sa stranice Carnet-a</a>
-    
-   </dd>
-    <dt>ad4</dt>
-  <dd>- Ova stranica je rađena uz pomoć navedene stranica</dd>
-   
-</dl> 
-
-</body>
-
-
-
-</html>
-
-
+<DOCTYPE html>
+    <html>
+        <heade>
+            <TITLE>Gdje radim</TITLE>
+            <link rel="stylesheet" type="text/css" href="Style.css">
+        </heade>
+        <body>
+              <!-- Traka izbornika -->
+            <ul>
+                <li><a class="active" href="#"><span>Početna</span></a></li>
+                <li><a href="Instalacije.php"><span>Instalacije</span></a></li>
+               <li><a href="#document">PHP Dokumentacija</a></li>
+               <li><a href="#link">Korisni Linkovi</a></li>
+               
+              
+               
+            </ul>   
+              <!-- tekst -->
+            <div style="margin-left:10%;padding:1px 16px;height:1000px;"> 
+                
+            
+            </div>
+            
+        </body>
+       
+        
+    </html>    
