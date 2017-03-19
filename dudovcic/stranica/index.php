08c4553c21019bb093b4d2b4a83768449dbbb789
dudovcic <denisudovcic@gmail.com>
2017-03-19 17:04:09 +0100

    stranica

---
+++ dudovcic/stranica/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <title>Stranica</title>
+    <link rel="stylesheet" href="css/styles.css">
+  </head>
+
+  <body>
+    <div id="container">
+
+      <div class="text">
+        <p class="parag">Random paragraph</p>
+        <span class="span"> Random span</span>
+      </div>
+
+      <div class="tablica">
+        <table>
+          <tr>
+            <td>Tekst u tablici</td>
+            <td>Tekst u tablici</td>
+          </tr>
+
+          <tr>
+            <td>Tekst u tablici</td>
+            <td>Tekst u tablici</td>
+          </tr>
+
+          <tr>
+            <td>Tekst u tablici</td>
+            <td>Tekst u tablici</td>
+          </tr>
+        </table>
+
+      </div>
+
+      <div id="slika">
+        <img src="http://lorempixel.com/output/animals-q-c-640-480-10.jpg">
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  </body>
+</html>
