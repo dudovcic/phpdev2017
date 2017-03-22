e4331447dea4c1f31e2ca0094708bd5a8ddf84cb
dENQ <qeqedenq@gmail.com>
2017-03-22 15:16:30 +0100

    full done

--- dudovcic/zadaca24/index.php
+++ dudovcic/zadaca24/index.php
@@ -1,81 +1,121 @@
 <?php
+echo "<br/>";echo "<br/>";
 
+echo "      Math ops";echo "<br/>";echo "<br/>";
+// Numeric ops
 
+// Int
+$a = 20;	
 
-$a = 20;
-$b = 33.2;
+// Double
+$b = 33.2; 
+
 
 $c = ( $a + $b) * ($b / $a);
-echo $c;
+echo $c;    echo "<br/>";
 
-// Indexed Array
+echo "<br/>";echo "<br/>";echo "<br/>";
+echo "________________________________________";
+echo "<br/>";echo "<br/>";
+echo "          Arrays";
 
-$indarr = array( 11, "random string", true, "last one" );
+// Arrays
 
-// Associative Array
 
-$assocarr = array( 
-	"first" 	=> "monday",
-	"second"	=> "tuesday",
-	"third"		=> "wednesday",
-	"fourth"	=> "thursday",
-	"fifth"		=> "friday",
-	"sixth"		=> "saturday",
-	"seventh"	=> "sunday"
-	 );
 
+	// Indexed Array
+
+	$indarr = array( 11, "random string", true, "last one" );
+
+	// Associative Array
+
+	$assocarr = array( 
+		"first" 	=> "monday",
+		"second"	=> "tuesday",
+		"third"		=> "wednesday",
+		"fourth"	=> "thursday",
+		"fifth"		=> "friday",
+		"sixth"		=> "saturday",
+		"seventh"	=> "sunday"
+		 );
+
+
+echo "<br/>";echo "<br/>";echo "<br/>";
 // Array print
 
+echo "<br/>";
+echo "  Index array";echo "<br/>";echo "<br/>";
 	// Printing Indexed
 
-	echo $indarr[0];
-	echo $indarr[3];
+	echo $indarr[0];        echo "<br/>";
+	echo $indarr[3];        echo "<br/>";
 
-
-
+echo "<br/>";
+echo "  Assoc array";echo "<br/>";echo "<br/>";
 	// Printing Associative
 
-	echo $assocarr["first"];
-	echo $assocarr["seventh"];
+	echo $assocarr["first"];        echo "<br/>";
+	echo $assocarr["seventh"];      echo "<br/>";
 
-
-
+echo "<br/>";
+echo "  Compl array";echo "<br/>";echo "<br/>";
 	// Complete Array
 
-	echo print_r($indarr);
-	echo print_r($assocarr);
+	echo print_r($indarr);      echo "<br/>";
+	echo print_r($assocarr);    echo "<br/>";  
 
 
-
+echo "<br/>";echo "<br/>";
+echo "  Array count()"; echo "<br/>";echo "<br/>";
 	// Array length
 
-	echo count($indarr);
-	echo count($assocarr);
+	echo count($indarr);    echo "<br/>";
+	echo count($assocarr);  echo "<br/>";
 
+
+
+echo "<br/>";echo "<br/>";echo "<br/>";
+echo "________________________________________";
+echo "<br/>";echo "<br/>";echo "<br/>";
+echo "          Ime - Prezime";
+echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";echo "<br/>";
 // Vars
 
 $ime 		= "Denis";
 $prezime 	= "Udovcic";
 
 $ask = "Dobar dan " . $ime . ", da li se vi prezivate " . $prezime . " ?";
-echo $ask;
+echo $ask;  echo "<br/>";
 
-echo strlen($ask);
+echo strlen($ask);  echo "<br/>";
 
 
-// --- BONUS ---
+
+
+echo "<br/>";echo "<br/>";echo "<br/>";
+
+echo "________________________________________";
+echo "<br/>";echo "<br/>";
+echo "          Bonus ";echo "<br/>";echo "<br/>";
+// 				--- BONUS ---
 
 
 $someArr = array();
 
 for ( $i = 0; $i < 777; $i++ ) {
 
-	array_push($someArr, genRandomString() );
+	array_push($someArr, genRandomString( rand(7, 15) ) );
 
 }
 
-printEveryX($someArr, 5);
+printEveryX($someArr);
 
+
+
+// Functions
+
+
+// Return random string with length 10 ( by deafult )
 
 function genRandomString($length = 10) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -87,7 +127,9 @@ function genRandomString($length = 10) {
     return $randomString;
 }
 
-function printEveryX(&$arr, $X) {
+// Print every 5th ( by default ) value from array
+
+function printEveryX(&$arr , $X = 5) {  /*  $arr passed by reference */ 
 	for ( $i = 0; $i < count($arr); $i+= $X) {
         echo $arr[$i]."<br/>";
 	}
