6625757ff9af25cee334317f65d20f9bed3c3ea7
dENQ <qeqedenq@gmail.com>
2017-03-22 13:27:02 +0100

    created vars and arrays

--- dudovcic/zadaca24/index.php
+++ dudovcic/zadaca24/index.php
@@ -2,11 +2,26 @@
 
 
 
+$a = 20;
+$b = 33.2;
 
+$c = ( $a + $b) * ($b / $a);
 
+// Index Arr
 
+$indarr = array( 11, "random string", true, "last one" );
 
+// Assoc Array
 
+$assocarr = array( 
+	"first" 	=> "monday",
+	"second"	=> "tuesday",
+	"third"		=> "wednesday",
+	"fourth"	=> "thursday",
+	"fifth"		=> "friday",
+	"sixth"		=> "saturday",
+	"seventh"	=> "sunday"
+	 )
 
 
 
