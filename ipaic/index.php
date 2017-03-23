0ec5cd686f1e5f10e252ee5c0c1f58c8d8840a51
paic <paic@i3.lan>
2017-03-23 22:24:11 +0100

    dz#1

---
+++ ipaic/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            table, th, td {border: 2px solid green;}
+            th, td {padding: 5px;}
+            th {text-align:left;}
+            table{border-spacing: 5px;}
+        </style>
+        <meta charset="UTF-8">
+        <title>ŠOPING</title>
+    </head>
+    <body>
+        <table>
+            <caption style="font-size:300%;">ŠOPING</caption>
+            <tr>
+                <th>Namirnica</th>
+                <th>Količina</th>
+                <th colspan="2">Trgovački Centar</th>
+                <th colspan="2">Cijena</th>
+            </tr>
+            <tr>
+                <th>Rajčica</th>
+                <th>2</th>
+                <th>Billa</th>
+                <th>Konzum</th>
+                <th>5</th>
+                <th>6</th>
+            </tr>
+            <tr>
+                <th>Mrkva</th>
+                <th>1</th>
+                <th>Interspar</th>
+                <th>Konzum</th>
+                <th>10</th>
+                <th>11</th>
+            </tr>
+            <tr>
+                <th>Jabuka</th>
+                <th>5</th>
+                <th>Billa</th>
+                <th>Interspar</th>
+                <th>3</th>
+                <th>2</th>
+            </tr>
+        </table>
+        
+    </body>
+</html>
