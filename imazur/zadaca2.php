b98affacf4f0da4680302a4724d95edaa44653a1
ivanmazur001 <ivanmazur001@gmail.com>
2017-03-24 00:16:30 +0100

    zad2

---
+++ imazur/zadaca2.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = (int) $a = 100.99;  
+$b = (double) $b = 99.09;
+
+echo $a;
+echo "<br>";
+echo $b;
+echo "<br>";
+echo $c = ($a+$b);
+echo "<br>";
+echo $c = ($a-$b);
+echo "<br>"; 
+echo $c = ($a*$b);
+echo "<br>";
+echo $c = ($a/$b);
+echo "<br>";
+echo $c = ($a%$b);
+echo "<br>";
+$c = (double) $c;
+        
+
+        
