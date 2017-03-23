22e4367dab839e9cb19034664e8e3e2f419d55e6
paic <paic@i3.lan>
2017-03-24 00:08:03 +0100

    dz#1

--- ipaic/dz2.php
+++ ipaic/dz2.php
@@ -1,8 +1,23 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$a= 5;
+$b= 6.0;
+$c= $a / $b;
+echo "Rezultat je: $c <br>";
+$auti = array("Ferari","BMW","Lambo");
+var_dump($auti);
+echo "<br>";
+$arrayduljina= count($auti);
+echo "Duljina arraya je:", $arrayduljina, "<br>";
+for($x = 0; $x < $arrayduljina; $x++) {
+    echo $auti[$x];
+    echo "<br>";    
+}
+$visina = array("Matko"=>"135", "Pero"=>"237", "Apolon"=>"43", "Stevo"=>"173");
+$visinaduljina= count($visina);
+var_dump($visina);
+echo "<br>";
+echo "Duljina ovog drugog je: $visinaduljina", "<br>";
+foreach ($visina as $obilježje =>$vrijednost ) {
+    echo "$obilježje $vrijednost <br>";
+}
+        ?>
