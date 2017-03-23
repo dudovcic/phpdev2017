658f0d2690639d021333dab5c5f0607310dc5de3
paic <paic@i3.lan>
2017-03-24 00:46:54 +0100

    dz#1

--- ipaic/dz2.php
+++ ipaic/dz2.php
@@ -20,4 +20,23 @@ echo "Duljina ovog drugog je: $visinaduljina", "<br>";
 foreach ($visina as $obilježje =>$vrijednost ) {
     echo "$obilježje $vrijednost <br>";
 }
+$ime = "Ivan";
+$prezime = "Paich";
+echo ("Valjd je ovo rješenje $ime problema $prezime");
+echo "<br>";
+echo "Duljina stringa je: ",strlen("Valjd je ovo rješenje $ime . problema  $prezime");
+echo "<br>";
+$bonus = array();
+$y= 0;
+for($x = 0; $x <= 777; $x++)
+{
+    $bonus[$x]= rand(0, 1000);
+    if(($x%5) == "0")
+    {
+        echo "$bonus[$x], <br>";
+        $y=$y+1;
+        
+    }
+}
+echo "Broj ispisanih članova: $y";
         ?>
