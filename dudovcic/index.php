06a6d6a615d574ce11a9f2d19e31de042404436d
iPaich <ipaic47@gmail.com>
2017-03-29 19:37:08 +0200

    Merge branch 'master' of https://github.com/algebrateam/phpdev2017
    # Conflicts:
    #	.gitignore

---
+++ dudovcic/index.php
@@ -0,0 +1,45 @@
+
+
+<html>
+    <body>
+        Hello from raw HTML <br/>
+<?php
+    for ( $i = 0; $i <= 500; $i++) {
+        
+        if ( !($i % 2) ) {
+        echo
+        "<li>
+              -    $i    phppp  </li>";
+            
+            
+        }
+    }
+
+?>
+        <form action="/dudovcic/index.php" method="POST">
+            <input type="text">
+            <input type="submit">
+            
+        </form>
+        
+        
+        
+   </body> 
+</html>
+<?php
+echo "Hello from php";
+echo"<br/>"
+
+
+
+
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+?>
