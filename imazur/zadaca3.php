8b71758028bc272f11d8f54f65547c03bf668315
ivanmazur001 <ivanmazur001@gmail.com>
2017-03-29 08:13:53 +0200

    z3

--- imazur/zadaca3.php
+++ imazur/zadaca3.php
@@ -10,7 +10,10 @@ $studenti = array (1 => "marko",
                    8 => "jelena",
                    9 => "vanja",
                   10 => "mario");
-echo strlen ($studenti{1});
+echo "<hr>";
+$f = array_filter(array_keys($studenti), function ($value){ return strlen($value)>5; });
+print_r($studenti);
+
 echo "<hr>";
 array_push($studenti,"ivana","mirta","melita","miro","bruno" );
 print_r($studenti);
@@ -28,10 +31,31 @@ echo "<pre>";
 print_r($studenti);
 echo "</pre>";
 
+array_walk($studenti, function(&$value)
+{
+  $value = ucfirst($value);
+});
+echo "<pre>";
+print_r($studenti);
+echo "</pre>";
 
-echo ucfirst("marko");
 
-
+$sneparni = array();
+$sparni = array();
+foreach( $studenti as $key => $value ) {
+    if( 0 === $key%2) { //Even
+        $sparni[] = $key;
+    }
+    else {
+        $sneparni[] = $key;
+    }
+}
+echo "<pre>";
+print_r($sparni);
+echo "</pre>";
+echo "<pre>";
+print_r($sneparni);
+echo "</pre>";
 
 
 /* 
