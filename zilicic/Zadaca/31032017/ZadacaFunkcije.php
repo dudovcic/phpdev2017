ccc27d5bcb9955364cc855b5ba64d12b1792c6cc
zilicic <zilicic5@h-1.hr>
2017-03-31 00:10:31 +0200

    Zadaca prvi i treci zadatak

--- zilicic/Zadaca/31032017/ZadacaFunkcije.php
+++ zilicic/Zadaca/31032017/ZadacaFunkcije.php
@@ -12,14 +12,21 @@ function hipo($x,$y){
     return sqrt($x*$x+$y*$y) ;
     }
 echo "<br>";
-echo hipo(3,4);
+echo 'Dužina hipotenuze je : '.hipo(3,4);
+echo'<br>';
 
 //Napraviti funkciju koja prima jednodimenzionalno polje i vraća polje kvadrata elemenata
-function poljeKvadrat()
-{}
-echo "<br>";
-
-
+function newArray(){
+//create an empty array
+    $newArray = array();
+    //methods for inserting values into a newly created array
+    $newArray[] = "a value";
+    $anotherArray = array( "value", "another value");
+    }
+    print newArray;
+function takes_array($arr){
+    
+}
 
 //Funkcija koja prima text i iz njega izbacuje sve samoglasnike
 
@@ -27,4 +34,5 @@ function izbaci($Str){
 $samo = array("a", "e", "i", "o", "u", "A", "E", "I", "O", "U", " ");
 		return str_replace($samo, "", $Str);
          }
-echo izbaci("Domaca");
+         
+echo izbaci("Zlatko");
