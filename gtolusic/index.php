5898857c9f3a4bfaf4a93ac53e2894c083bd4447
gorant696 <axelfoley@net.hr>
2017-03-30 22:22:20 +0200

    comitanje

--- gtolusic/index.php
+++ gtolusic/index.php
@@ -1,42 +1,47 @@
 <?php
-//array studenti
-$student = array("domagoj", "ana", "ivo", "tomislav", "ivica", "ena", "zvonimir", "ivana", "zrinka", "marko");
-print_r($student);
-echo "<br>";
-//s manje od 5 znakova imena
-$b= array_filter($student,"petica");
-print_r($b);
-echo "<br>";
-function petica($x)
-{
-    if (strlen($x) < 5) {return ($x);}
-    
-};
-//dodavanje 5 studenata
-array_push($student, "jurica", "anela", "dijana", "hana", "damir");
-print_r($student);
-echo "<br>";
-//sortiranje
-sort($student);
-print_r($student);
-echo "<br>";
-//ubijem zadnja 2
-array_pop($student);
-array_pop($student);
-print_r($student);
-echo "<br>";
-//s velikim slovima
-foreach ($student as $s) {
-    echo(ucfirst($s))."<br>";
-};
-echo "<br>";
-//temperature
-$temperatura= array (1,2,3,4,5,6,7,8,9,10);
-echo min($temperatura)."<br>";
-echo max($temperatura)."<br>";
-echo   array_sum($temperatura)/count($temperatura);
 
-//iskreno zadatak sa neparnim i parnim studentima nisam dobro shvatio. Jel parni moraju biti keyevi ili broj karaktera? :/
+//hipotenuza
+function hipo($x,$y) {
+        echo "ako je dužina prve katete:".$x. " i dužina druge katete:".$y." Onda je dužina hipotenuze:".hypot($x,$y);
+         
+    }
     
-
+    hipo(6,7);
+    
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    echo "<br>";
+    
+    //kvadrati iz arraya
+    
+    $brojevi = array (2,4,5,6);
+   
+   
+    function kvadrati($brojevi)  {      
+             foreach ($brojevi as $p){
+    $kvadrat = $p*$p;
+     print_r($kvadrat);
+              echo "<hr>";
+    
+             }
+    }
+    
+    kvadrati($brojevi);
+    
+    //samoglasnici
+   
+     $abc="ovo je samo sa malim suglasnicima";
+    echo samo($abc);
+   
+    function samo($recenica){
+                $samoglasnici=array ("q", "w", "r", "t", "z", "p", "š", "đ" ,"s", "d", "f", " ", "g", "h", "j", "k", "l","č", "ć", "ž", "x", "c","v","b","n","m");
+                
+                              
+        echo str_replace($samoglasnici,"", $recenica);
+        
+                    
+    }
+   
+  
 ?>
