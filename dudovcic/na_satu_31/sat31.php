116f341a8889cae1a796c24d2bbcd52fa9fd44fe
dENQ <qeqedenq@gmail.com>
2017-03-31 21:40:42 +0200

    ...

--- dudovcic/na_satu_31/sat31.php
+++ dudovcic/na_satu_31/sat31.php
@@ -1,19 +1,20 @@
 <?php 
 
-function main($oper, $a, $b) {
+function main($oper, &$a, &$b) {
+
 
 	switch($oper) {
-		case "zbr":
+		case "zbroj":
 			return zbr($a, $b);
 			break;
-		case "zbr":
+		case "razlika":
 			return razl($a, $b);
 			break;
-		case "zbr":
+		case "umnozak":
 			return mno($a, $b);
 			break;
-		case "zbr":
-			return dij($a, $b);
+		case "podjela":
+			return dij();
 			break;
 		default: return "Incorrect operation";
 
@@ -27,21 +28,38 @@ function zbr($a, $b) {
 
 
 
-function razl($a, $b) {
+function razl(&$a, &$b) {
 	return $a - $b;
 }
 
 
 
 function mno($a, $b) {
-	return $a * $b;
+	static $x ;
+	$x = $a;
+	static $y ;
+	$y = $b;
+
+	return $x * $y;
 }
 
 
 
-function dij($a, $b) {
+function dij() {
+	global $a;
+	global $b;
 	return $a / $b;
 }
 
+$a = 20;
+$b = 30;
+
+echo main("zbroj", $a, $b);
+echo "<br>";
+echo main("razlika", $a, $b);
+echo "<br>";
+echo main("podjela", $a, $b);
+echo "<br>";
+echo main("umnozak", $a, $b);
 
  ?>
