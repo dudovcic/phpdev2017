d62512eda5a1c8fad1992c7bf435e717e5ab51fc
bvidov45 <bvidovic45@gmail.com>
2017-03-31 11:50:09 +0200

    zadatak hipotenuza

---
+++ bvidovic/zad3103.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>Php</title>
+            
+    </head>
+    
+    <body>
+ <?php
+    # zadaca hipotenuza
+    function hipo($kat1, $kat2){
+        return sqrt($kat1*$kat1+$kat2*$kat2);
+    }
+    echo "Hiptenuza je : " . hipo(4,3);
+    echo "<br>";
+     
+   
+   
+       
+   
+  
+        
+ ?>
+        
+        
+    </body>
+</html>
+
