e2a682a74ff6234cb9ebd81824906344a3487392
bvidov45 <bvidovic45@gmail.com>
2017-04-01 09:55:55 +0200

    zadaca za 31.03

--- bvidovic/zad3103.php
+++ bvidovic/zad3103.php
@@ -6,20 +6,33 @@
     
     <body>
  <?php
-    # zadaca hipotenuza
+       
+        # zadaca hipotenuza
     function hipo($kat1, $kat2){
         return sqrt($kat1*$kat1+$kat2*$kat2);
     }
-    echo "Hiptenuza je : " . hipo(4,3);
+    echo "Hipotenuza je : " . hipo(4,3);
     echo "<br>";
      
-   
-   
-       
-   
+    # funkcija sa samoglasnicima
+ 
+   $tekst = "Povijest knjige stara je više od 7 000 godina. U tome vremenu mijenjala je svoj oblik";
+     $smgls = array('a', 'e', 'i', 'o', 'u', );
+     $obrisati = str_replace($smgls, '', $tekst);
+     echo $obrisati; 
+     echo"<br>";
   
-        
- ?>
+    #polje kvadrata elementa
+     
+     function nakvadrat($i)
+  {
+    return($i * $i);
+  }
+  $a = array(3, 5, 7, 9);
+  $b = array_map("nakvadrat", $a);
+   print_r($b);
+  
+    ?>
         
         
     </body>
