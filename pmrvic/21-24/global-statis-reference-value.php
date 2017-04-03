b9ef7f9c2ce8a867bdb0abc37bd52151c77752b3
mrvic <mrvic.predrag@gmail.com>
2017-04-03 14:51:21 +0200

    Zadaća za 03.04.2017 Napraviti operaciju zbrajanja, oduzimanja, množenja i djeljenja pomoću funkcija, koristeći, VALUE, REFERENCE,GLOBAL,STATIC

--- pmrvic/21-24/global-statis-reference-value.php
+++ pmrvic/21-24/global-statis-reference-value.php
@@ -1,30 +1,48 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// napravi nfunkciju koja zbraja dva broja a argumente prima po vrijednosti
 function zbr($a, $b) {
 	return $a + $b;
 }
-function razl($a, $b) {
-	return $a - $b;
+printf("zbroj a=%d i b=%d je %d<br>", $a=3,$b=9, zbr($a,$b));
+
+
+// napravi nfunkciju koja zbraja dva broja a argumente prima po referenci
+$a=25;
+$b=15;
+function razl(&$a, &$b) {
+	$a -= $b;       // nije potrebno return, rezultat je već u globalnom $a
 }
-$z=0;
-// sa static
-function mno($a, $b) {
-    static $y=$b;
-    static $z=0;
-    $z+=$a; 
-    while($y>0){
-    $b--; 
-    echo $b." ".$z;
-    mno($a,$b);
+printf("razlika a=%d i b=%d", $a,$b);
+razl($a,$b);
+printf(" je %d<br>", $a);
+
+
+
+// Pomoću static metode napraviti funkciju koja množi dva broja uz pomoć zbrajanja
+function mno($a, $b=0) {  
+    static $y=0;        // broj ponavljanja
+    static $z=0;        // privremeni rezulat zbroja
+    if($b!=0){          // Ukoliko je broj ponavljanja veći od nule 
+     $y=$b;    
     }
-    if ($b<=0) return $z;
+    $z+=$a;             // pri svakom pozivu funkcije pribroji rezultatu
+    $y--;               // umanji broj ponavljanja
+    while($y>0){        // rekurzijom pozivaj funkciju
+      mno($a);  
+    }
+    return $z;          // na kraju vrati ukupan zbroj (u biti konačni umnožak)
 }
-function dij($a, $b) {
-	return $a / $b;
+
+printf("umnožak <b>a=%d i b=%d</b> je %d<br>", $a=6,$b=7, mno($a,$b));
+
+
+
+$a=56;
+$b=8;
+function dij() {
+    global $a,$b;
+   // echo $a."".$b."aaaaa<br>";
+    return $a /= $b;
 }
-echo mno(3,3);
+printf("kvocijent od <b>a=%0.0f</b> i <b>b=%0.0f</b> je %f<br>", $a,$b, dij());
