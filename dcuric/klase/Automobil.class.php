e5c81b86a573a6473976056962867001432178e6
curicdomagoj <curicdomagoj993@gmail.com>
2017-04-06 19:22:36 +0200

    dodajem klase sa sata

---
+++ dcuric/klase/Automobil.class.php
@@ -0,0 +1,19 @@
+<?php
+
+interface IAuto{
+function vozi(); 
+function stani(); 
+function skreni(); 
+function gazi();
+}
+
+interface IAutomobil{
+    function get_model();
+}
+
+class Auto{
+   
+}
+
+
+?>
