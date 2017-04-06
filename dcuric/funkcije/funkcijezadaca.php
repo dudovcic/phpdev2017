7df39df2d7b4c901271279723d211cfa218e311b
curicdomagoj <curicdomagoj993@gmail.com>
2017-04-06 19:18:48 +0200

    izmijenio sam jedan dio zadatka

--- dcuric/funkcije/funkcijezadaca.php
+++ dcuric/funkcije/funkcijezadaca.php
@@ -11,19 +11,22 @@ echo "<hr>";
 function kvadrat($x){
     return $x*$x;
 }
-
+//spremit u polje i vratit polje,array map
 function polje_kvadrata($array1){
     for($i=0;$i<count($array1);$i++){
-        $fa=$array1[$i]*$array1[$i];
-        echo $fa." ";
+        $array1[$i]=$array1[$i]*$array1[$i];
+        
     }
+    return $array1;
 }
 
 $p1=array(8,5,6,7,23,45,6,7,89,99,0,1,999);
-echo polje_kvadrata($p1);
+print_f($p1);
+echo "<br>";
+
 
 echo "<hr>";
-
+//regular_expres 2.opcijes
 function text($string){ 
   $string = preg_replace('/(?:[aeiou]|(?<=[bcdfghjklmnpqrstvwxyz]))/','', $string); 
    return $string;
