6d895a98e76300207965c2a336759049785a01a4
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:11:50 +0200

    dodana tablica sefovi

--- bvidovic/bazapodataka/Zaposlenici.sql
+++ bvidovic/bazapodataka/Zaposlenici.sql
@@ -19,3 +19,20 @@ CREATE TABLE `zaposlenici` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
+CREATE TABLE `sefovi` (
+	`Sifra_zaposlenika` BIGINT(20) NOT NULL DEFAULT '0',
+	`Sifra_odjela` CHAR(10) NOT NULL DEFAULT '',
+	PRIMARY KEY (`Sifra_zaposlenika`, `Sifra_odjela`),
+	INDEX `Sifra_zaposlenika` (`Sifra_zaposlenika`),
+	INDEX `Sifra_odjela` (`Sifra_odjela`),
+	CONSTRAINT `Sefovi_ibfk_1` FOREIGN KEY (`Sifra_odjela`) REFERENCES `odjeli` (`Sifra_odjela`),
+	CONSTRAINT `Sefovi_ibfk_2` FOREIGN KEY (`Sifra_zaposlenika`) REFERENCES `zaposlenici` (`Sifra_zaposlenika`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
+
+
+
+
+
