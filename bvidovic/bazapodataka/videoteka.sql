0fc7cc9284836729ecac8c5083db45b334b6426c
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:31:22 +0200

    kreirana baza podataka videoteka i dodana tablica clanovi

---
+++ bvidovic/bazapodataka/videoteka.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `clanovi` (
+	`Clanski_broj` INT(11) NOT NULL,
+	`Ime` VARCHAR(30) NULL DEFAULT NULL,
+	`Prezime` VARCHAR(30) NULL DEFAULT NULL,
+	`Adresa` VARCHAR(30) NULL DEFAULT NULL,
+	`Telefon` CHAR(15) NULL DEFAULT NULL,
+	`Datum_uclanjenja` DATETIME NULL DEFAULT NULL,
+	PRIMARY KEY (`Clanski_broj`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
