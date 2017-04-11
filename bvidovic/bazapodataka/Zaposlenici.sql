15d57a5c29981737cca76848162a14f75353eb08
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:09:10 +0200

    dodana tablica zaposlenici

--- bvidovic/bazapodataka/Zaposlenici.sql
+++ bvidovic/bazapodataka/Zaposlenici.sql
@@ -7,3 +7,15 @@ CREATE TABLE `odjeli` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
+CREATE TABLE `zaposlenici` (
+	`Sifra_zaposlenika` BIGINT(20) NOT NULL,
+	`Ime` VARCHAR(50) NOT NULL,
+	`Prezime` VARCHAR(50) NOT NULL,
+	`Sifra_odjela` CHAR(10) NOT NULL,
+	PRIMARY KEY (`Sifra_zaposlenika`),
+	INDEX `Sifra_odjela` (`Sifra_odjela`),
+	CONSTRAINT `Zaposlenici_ibfk_1` FOREIGN KEY (`Sifra_odjela`) REFERENCES `odjeli` (`Sifra_odjela`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
