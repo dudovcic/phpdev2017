71fca760ebe47477ac0b6269f63cbd28105db994
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:36:42 +0200

    dodana tablica filmovi

--- bvidovic/bazapodataka/videoteka.sql
+++ bvidovic/bazapodataka/videoteka.sql
@@ -18,3 +18,20 @@ CREATE TABLE `zanr` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
+CREATE TABLE `filmovi` (
+	`Sifra_filma` INT(11) NOT NULL,
+	`Naziv` VARCHAR(30) NULL DEFAULT NULL,
+	`Reziser` VARCHAR(30) NULL DEFAULT NULL,
+	`Glavni_glumci` VARCHAR(100) NULL DEFAULT NULL,
+	`Godina_izdanja` INT(11) NULL DEFAULT NULL,
+	`Kolicina_dvd` INT(11) NULL DEFAULT NULL,
+	`Kolicina_vhs` INT(11) NULL DEFAULT NULL,
+	`Slika_naslovnice` BLOB NULL,
+	`Sifra_zanra` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`Sifra_filma`),
+	INDEX `Sifra_zanra` (`Sifra_zanra`),
+	CONSTRAINT `Filmovi_ibfk_1` FOREIGN KEY (`Sifra_zanra`) REFERENCES `zanr` (`Sifra_zanra`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
