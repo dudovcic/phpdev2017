d4fbc520a501a8be7d17a04f9a8047e100e6fb2f
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:21:36 +0200

    kreirana tablica radnik

--- bvidovic/bazapodataka/skladiste.sql
+++ bvidovic/bazapodataka/skladiste.sql
@@ -7,3 +7,15 @@ CREATE TABLE `skladisno_mjesto` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
+CREATE TABLE `radnik` (
+	`Sifra_radnika` INT(11) NOT NULL,
+	`Ime` CHAR(30) NULL DEFAULT NULL,
+	`Prezime` CHAR(30) NULL DEFAULT NULL,
+	`Sifra_skladista` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`Sifra_radnika`),
+	INDEX `Sifra_skladista` (`Sifra_skladista`),
+	CONSTRAINT `Radnik_ibfk_1` FOREIGN KEY (`Sifra_skladista`) REFERENCES `skladisno_mjesto` (`Sifra_skladista`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
