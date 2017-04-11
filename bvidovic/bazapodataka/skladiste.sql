95c3dbd39242d419421df52295412b4ac98d6b70
bvidov45 <bvidovic45@gmail.com>
2017-04-11 13:23:04 +0200

    kreirana tablica proizvod

--- bvidovic/bazapodataka/skladiste.sql
+++ bvidovic/bazapodataka/skladiste.sql
@@ -19,3 +19,14 @@ CREATE TABLE `radnik` (
 COLLATE='latin1_swedish_ci'
 ENGINE=InnoDB
 ;
+CREATE TABLE `proizvod` (
+	`Sifra_proizvoda` INT(11) NOT NULL,
+	`Naziv` CHAR(30) NULL DEFAULT NULL,
+	`Sifra_skladista` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`Sifra_proizvoda`),
+	INDEX `Sifra_skladista` (`Sifra_skladista`),
+	CONSTRAINT `Proizvod_ibfk_1` FOREIGN KEY (`Sifra_skladista`) REFERENCES `skladisno_mjesto` (`Sifra_skladista`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
