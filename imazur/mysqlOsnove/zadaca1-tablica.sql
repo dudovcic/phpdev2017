928f79ce2653b4528c978a318407240ceadb40b5
ivanmazur001 <ivanmazur001@gmail.com>
2017-04-12 08:47:18 +0200

    zadatak2-baza i tri tablice

--- imazur/mysqlOsnove/zadaca1-tablica.sql
+++ imazur/mysqlOsnove/zadaca1-tablica.sql
@@ -1,6 +1,8 @@
-/* zadatak 1
+/* zadatak 2.1
 
 */
+
+CREATE DATABASE `gbm`;
 CREATE TABLE `odjeli` (
 	`ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
 	`odjel` VARCHAR(55) NOT NULL,
@@ -51,3 +53,58 @@ INSERT INTO `odjeli` (`ID`, `odjel`) VALUES
 
 --------------------------------------------
 
+/* zadatak 2.2
+
+*/
+
+Microsoft Windows [Version 6.1.7601]
+Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+
+C:\>cd xampp
+
+C:\xampp>cd mysql
+
+C:\xampp\mysql>cd bin
+
+C:\xampp\mysql\bin>mysql -u root -p
+Enter password:
+Welcome to the MariaDB monitor.  Commands end with ; or \g.
+Your MariaDB connection id is 3
+Server version: 10.1.21-MariaDB mariadb.org binary distribution
+
+Copyright (c) 2000, 2016, Oracle, MariaDB Corporation Ab and others.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+MariaDB [(none)]> USE trgovina
+Database changed
+
+MariaDB [trgovina]> CREATE TABLE `skladiste1` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `materijal` VARCHAR(55) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.28 sec)
+
+MariaDB [trgovina]> CREATE TABLE `skladiste2` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `alat` VARCHAR(55) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.42 sec)
+
+MariaDB [trgovina]> CREATE TABLE `skladiste3` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `ostalo` VARCHAR(55) NOT NULL,
+    ->
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.34 sec)
+
+MariaDB [trgovina]>
