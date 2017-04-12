42abe1b4f192f0c4790bf005baffaeea8c6844c5
ivanmazur001 <ivanmazur001@gmail.com>
2017-04-12 09:19:49 +0200

    zadatak2-unos u tablice

--- imazur/mysqlOsnove/zadaca1-tablica.sql
+++ imazur/mysqlOsnove/zadaca1-tablica.sql
@@ -25,7 +25,9 @@ CREATE TABLE `voditelji` (
 	`ID` INT(11) NOT NULL,
 	`ime` VARCHAR(55) NOT NULL,
 	`prezime` VARCHAR(79) NOT NULL,
-	`odjel` VARCHAR(79) NOT NULL
+	`odjel` VARCHAR(79) NOT NULL,
+    PRIMARY KEY (`ID`)
+
 )
 ENGINE=InnoDB
 ;
@@ -107,4 +109,107 @@ TO_INCREMENT,
     -> ;
 Query OK, 0 rows affected (0.34 sec)
 
-MariaDB [trgovina]>
+MariaDB [trgovina]>Microsoft Windows [Version 6.1.7601]
+Copyright (c) 2009 Microsoft Corporation.  All rights reserved.
+
+C:\Users\Korisnik>cd..
+
+C:\Users>cd..
+
+C:\>cd xampp
+
+C:\xampp>cd mysql
+
+C:\xampp\mysql>cd b
+The system cannot find the path specified.
+
+C:\xampp\mysql>cd bin
+
+C:\xampp\mysql\bin>mysql -u root -p
+Enter password:
+Welcome to the MariaDB monitor.  Commands end with ; or \g.
+Your MariaDB connection id is 3
+Server version: 10.1.21-MariaDB mariadb.org binary distribution
+
+Copyright (c) 2000, 2016, Oracle, MariaDB Corporation Ab and others.
+
+Type 'help;' or '\h' for help. Type '\c' to clear the current input statement.
+
+MariaDB [(none)]> USE trgovina
+Database changed
+MariaDB [trgovina]> CREATE TABLE `odjeli` (
+    ->  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMEN;
+ERROR 1064 (42000): You have an error in your SQL syntax; check the manual that
+corresponds to your MariaDB server version for the right syntax to use near '' a
+t line 2
+MariaDB [trgovina]> CREATE TABLE `skladiste1` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `materijal` VARCHAR(55) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.28 sec)
+
+MariaDB [trgovina]> CREATE TABLE `skladiste2` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `alat` VARCHAR(55) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.42 sec)
+
+MariaDB [trgovina]> CREATE TABLE `skladiste3` (`ID` INT(10) UNSIGNED NOT NULL AU
+TO_INCREMENT,
+    ->  `ostalo` VARCHAR(55) NOT NULL,
+    ->
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.34 sec)
+
+MariaDB [trgovina]> CREATE TABLE `zaposlenici` (
+    ->  `ID` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+    -> `ime` VARCHAR(55) NOT NULL,
+    -> `prezime` VARCHAR(77) NOT NULL,
+    -> `skladiste` VARCHAR(55) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ENGINE=InnoDB
+    -> ;
+Query OK, 0 rows affected (0.69 sec)
+
+MariaDB [trgovina]> CREATE TABLE `voditelji` (
+    -> `ID` INT(11) NOT NULL,
+    -> `skladiste` VARCHAR(79) NOT NULL,
+    -> `ime` VARCHAR(55) NOT NULL,
+    -> `prezime` VARCHAR(79) NOT NULL,
+    -> PRIMARY KEY (`ID`)
+    -> )
+    -> ;
+Query OK, 0 rows affected (0.25 sec)
+
+
+MariaDB [trgovina]> INSERT INTO `zaposlenici` (`ID`, `ime`, `prezime`, `skladist
+e`) VALUES
+    -> (1, 'ivo','ivoo', 'skladiste1'),
+    -> (2, 'maja','aio', 'skladiste1'),
+    -> (3, 'eva','evo', 'skladiste1'),
+    -> (4, 'simo','evo', 'skladiste2'),
+    -> (5, 'rudi','evo', 'skladiste2'),
+    -> (6, 'pero','evic', 'skladiste2'),
+    -> (7, 'stipo','evic', 'skladiste3'),
+    -> (8, 'dodo','ivic', 'skladiste3'),
+    -> (9, 'ana','ivic', 'skladiste3');
+Query OK, 9 rows affected (0.08 sec)
+Records: 9  Duplicates: 0  Warnings: 0
+
+MariaDB [trgovina]>INSERT INTO `voditelji` (`ID`,`skladiste`, `ime`, `prezime`)
+ VALUES
+    -> (1,'skladiste3', 'ana','ivic'),
+    -> (2,'skladiste2', 'pero','evic'),
+    -> (3,'skladiste1', 'eva','evo');
+Query OK, 3 rows affected (0.05 sec)
+Records: 3  Duplicates: 0  Warnings: 0
