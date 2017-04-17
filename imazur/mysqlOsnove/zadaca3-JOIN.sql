cb4c4ade3218b98fe29b5c188a07517c96182a56
ivanmazur001 <ivanmazur001@gmail.com>
2017-04-18 00:40:41 +0200

    zad3-str24

--- imazur/mysqlOsnove/zadaca3-JOIN.sql
+++ imazur/mysqlOsnove/zadaca3-JOIN.sql
@@ -15,3 +15,21 @@ FROM stud JOIN ispit
 ON stud.mbrStud=ispit.mbrStud
 AND ispit.ocjena=1
 AND ispit.sifPred BETWEEN 220 AND 240;
+
+/*zadatak 3.13.*/
+USE fakultet;
+SELECT DISTINCT  imeStud, prezStud
+FROM stud JOIN ispit ON stud.mbrStud=ispit.mbrStud
+WHERE ocjena=3;
+
+/*zadatak 3.14.*/
+USE fakultet;
+SELECT DISTINCT nazPred
+FROM pred JOIN ispit ON ispit.sifPred = pred.sifPred;
+
+/*zadatak 3.15.*/
+USE fakultet;
+SELECT nazPred
+FROM pred LEFT JOIN ispit ON pred.sifPred=ispit.sifPred
+WHERE datispit is NULL
+ORDER BY nazPred;
