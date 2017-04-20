d9fe7241f86a674be48bb66ae6da3cb42ff90284
curicdomagoj <curicdomagoj993@gmail.com>
2017-04-20 17:51:01 +0200

    dodajem jos nekolika zadatak u file zadnja dva nisam rijesio,updete 0, agregatne 0

--- dcuric/mysql/zadaca 18,4.sql
+++ dcuric/mysql/zadaca 18,4.sql
@@ -84,6 +84,83 @@ OR stud.prezStud LIKE "%nk%"
 ;
 
 
+/* 3.15 */
+SELECT stud.imeStud,stud.prezStud,pred.nazPred,ispit.ocjena
+FROM ispit
+JOIN stud  ON  stud.mbrStud=ispit.mbrStud
+JOIN pred  ON  pred.sifPred=ispit.sifPred
+;
+
+/* 3.16 */
+SELECT stud.imeStud,stud.prezStud,mjesto.nazMjesto AS "mjesto rođenja",zupanija.sifZupanija AS "zupanija rođenja",mjesto.nazMjesto AS "mjesto prebivalista",zupanija.nazZupanija AS "zupanija prebivališta"
+FROM stud 
+JOIN mjesto ON mjesto.pbr=stud.pbrRod
+JOIN zupanija ON zupanija.sifZupanija=mjesto.sifZupanija
+JOIN mjesto m1 ON m1.pbr=stud.pbrStan
+JOIN zupanija z1 ON z1.sifZupanija=mjesto.sifZupanija
+;
+
+
+
+/*3.17*/
+SELECT pred.nazPred,orgjed.nazOrgjed
+FROM pred
+JOIN orgjed ON pred.sifOrgjed=orgjed.sifOrgjed
+WHERE pred.upisanoStud>20
+;
+
+/*3.18*/
+SELECT DISTINCT mjesto.nazMjesto
+FROM mjesto
+JOIN stud ON mjesto.pbr=stud.pbrStan
+;
+
+/* 3.19 */
+SELECT DISTINCT mjesto.nazMjesto
+FROM mjesto
+JOIN stud ON mjesto.pbr=stud.pbrStan
+WHERE stud.pbrStan=stud.pbrRod
+;
+
+/* 3.20 */
+SELECT rezervacija.oznDvorana,pred.nazPred,pred.brojSatiTjedno
+FROM pred
+ JOIN rezervacija ON pred.sifPred=rezervacija.sifPred
+WHERE pred.brojSatiTjedno>2
+;
+
+/*3.21*/
+SELECT DISTINCT pred.nazPred,rezervacija.oznDvorana,orgjed.nazOrgjed
+FROM pred
+JOIN rezervacija ON pred.sifPred=rezervacija.sifPred
+JOIN orgjed ON pred.sifOrgjed=orgjed.sifOrgjed
+;
+
+/*3.22*/
+SELECT stud.imeStud,stud.prezStud
+FROM stud
+JOIN mjesto m1 ON stud.pbrRod = m1.pbr
+JOIN mjesto m2 ON stud.pbrStan = m2.pbr
+WHERE stud.pbrRod != stud.pbrStan
+AND m1.sifZupanija=m2.sifZupanija
+;
+
+/*3.23*/
+SELECT DISTINCT nastavnik.imeNastavnik,stud.imeStud,nastavnik.pbrStan,stud.pbrStan
+FROM nastavnik
+JOIN ispit ON ispit.sifNastavnik=nastavnik.sifNastavnik
+JOIN stud ON ispit.mbrStud=stud.mbrStud
+JOIN mjesto m1 ON m1.pbr=stud.pbrStan
+JOIN mjesto m2 ON m2.pbr=stud.pbrStan
+WHERE m1.sifZupanija=m2.sifZupanija
+;
+
+
+/*3.24*/
+SELECT * 
+FROM stud,nastavnik
+WHERE stud.prezStud=nastavnik.prezNastavnik
+;
 
 
 
