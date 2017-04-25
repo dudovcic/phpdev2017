37a02afd0f4cb21d383b482043affa1a0d539132
bvidov45 <bvidovic45@gmail.com>
2017-04-25 18:15:29 +0200

    dodana vjezbbe s predavanja

---
+++ bvidovic/stranica1.php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+       
+    </head>
+    <body>
+            <form method="POST" action="stranica2.php">
+            broj polja<input type="number" name="brojimena"/><br>
+            <br>
+            <input type="submit" value="Stisni"/>
+            
+            
+        </form> 
+        
+        
+    </body>
+</html>
