f9cd225672476f43e47ada8da56392f9e8fa02d9
paic <paic@i3.lan>
2017-04-26 05:04:03 +0200

    dz 26.4

--- ipaic/korak.php
+++ ipaic/korak.php
@@ -4,16 +4,19 @@
 	<title>DZ 164</title>
 </head>
 
-
-<form method="POST" action="upload.php" enctype="multipart/form-data" multiple>
-<br /> <input type="file" name="datoteka" id="datoteka" /><br />
-<br /> <input type="submit" name="upload_btn" value="Upload"> <br />
-
 <?php
-if(isset($_FILES['file']['name'][$_POST["broj"]]))
+
+for($j=0;$j<$_POST["broj"];$j++)
 {
-     exit("nemozete uploadati toliko fileova");
+   ?>
+<form method="POST" action="upload.php" enctype="multipart/form-data" multiple>
+
+<br /> <input type="file" name="file[]" multiple /><br />
+
+<?php 
+
 }
 ?>
-   
+broj datoteka: <br /> <input type="number" name="broj" min="1" max="5"  /><br />
+<br /> <input type="submit" name="upload_btn" value="Upload"> <br />
 </form>
--- ipaic/upload.php
+++ ipaic/upload.php
@@ -1,13 +1,17 @@
 <?php
+
+for($i=0;$i<$_POST["broj"];$i++){
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["datoteka"]["name"]);
+$target_file = $target_dir . basename($_FILES["file"]["name"][$i]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-
- if (move_uploaded_file($_FILES["datoteka"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["datoteka"]["name"]). " has been uploaded.";
+ if (move_uploaded_file($_FILES["file"]["tmp_name"][$i], $target_file)) {
+        echo "The file ". basename( $_FILES["file"]["name"][$i]). " has been uploaded.";
+        echo "<br>";
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
+}
+print_r($_FILES);
 ?>
 
