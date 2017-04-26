08c6b737dc40b873889ba8eb1d134d5ddc03008e
gorant696 <axelfoley@net.hr>
2017-04-26 18:59:58 +0200

    ispravak cetvrtog zadatka

--- gtolusic/uploadanje.php
+++ gtolusic/uploadanje.php
@@ -1,27 +1,29 @@
 
-<html>
 
-<?php   /*
-if (isset($_POST['submit']) || $_POST['broj']<=5)
 
-{for ($i=1;$i<=$_POST['broj'];$i++){
+<?php   
+if (isset($_POST['submit']) && $_POST['broj']<=5)
+
+{
+    for ($i=1;$i<=$_POST['broj'];$i++){
 ?>
 
 <form method="POST" action="" enctype="multipart/form-data">
 fajl za upload: 
-    <input type="file" name="fajl" value=""><br> <?php> } ?>
+    <input type="file" name="fajl[]" value=""><br> 
+<?php } ?>
     <input type="submit" name="upload_btn" value="upload">
     
-    
 </form>
+<?php  }
 
-<?php }
+
 
  
 
 if (isset($_POST['upload_btn']))
     {
-print_r($_FILES);
+print_r($_FILES['fajl']);
 
 
 
@@ -30,6 +32,5 @@ print_r($_FILES);
 
 
 
-?>   </html>
+?>   
 
-*/
