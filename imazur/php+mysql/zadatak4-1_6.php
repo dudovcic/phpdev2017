8f389ba15bf881b82480a15f0ff134ada0b1e333
ivanmazur001 <ivanmazur001@gmail.com>
2017-04-26 20:00:29 +0200

    zadatak4-prvi dio

--- imazur/php+mysql/zadatak4-1_6.php
+++ imazur/php+mysql/zadatak4-1_6.php
@@ -1,5 +1,11 @@
 <?php
+if (!file_exists('path/to/directory')) {
+    mkdir('path/to/directory', 0777, true);
+}
 
+
+
+move_uploaded_file(); 
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
