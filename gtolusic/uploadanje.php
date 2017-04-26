145185af5db163d7e6c403dd12aca9709eefdf79
gorant696 <axelfoley@net.hr>
2017-04-26 12:30:58 +0200

    upload pokusaj

--- gtolusic/uploadanje.php
+++ gtolusic/uploadanje.php
@@ -1,15 +1,33 @@
 
+<html>
+
+<?php   
+if (isset($_POST['submit']) || $_POST['broj']<=5)
+
+{for ($i=1;$i<=$_POST['broj'];$i++){
+?>
 
 <form method="POST" action="" enctype="multipart/form-data">
-    fajl za upload:<br>
-    <input type="file" name="fajl" value=""><br>
+fajl za upload: 
+    <input type="file" name="fajl" value=""><br> <?php> } ?>
     <input type="submit" name="upload_btn" value="upload">
     
     
 </form>
 
-<?php
+<?php }
 
-if (isset($_POST['upload_btn'])){
-print_r($_FILES);}
-?>
+ 
+
+if (isset($_POST['upload_btn']))
+    {
+print_r($_FILES);
+
+
+
+}
+
+
+
+
+?>   </html>
---
+++ gtolusic/uploadanje_1.php
@@ -0,0 +1,9 @@
+<form method="post" action="uploadanje.php">
+    
+    <input type="number" name="broj" value="">
+    <input type="submit" name="submit" value="submit">
+    
+    
+    
+    
+</form>
