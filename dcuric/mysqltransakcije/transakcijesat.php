a65fdd54f972e8ed83c2417cee3074894140df1d
curicdomagoj <curicdomagoj993@gmail.com>
2017-04-28 21:57:03 +0200

    malo steka

--- dcuric/mysqltransakcije/transakcijesat.php
+++ dcuric/mysqltransakcije/transakcijesat.php
@@ -4,22 +4,25 @@ include("db_connection.php");
 
 function begin()
 {
+    global $conn;
     $query="BEGIN";
-    $result= mysqli_query($query);
+    $result= mysqli_query($conn,$query);
             
 }
 
 function commit()
 {
+    global $conn;
     $query="COMMIT";
-    $result=mysqli_query($query);
+    $result=mysqli_query($conn,$query);
     
 }
 
 function rollback()
 {
+    global $conn;
     $query="ROLLBACK";
-    $result=mysqli_query($query);
+    $result=mysqli_query($conn,$query);
     
 }
 
@@ -27,29 +30,35 @@ begin();
 
 $error_num=0;
 
-$query="INSERT INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES ('23000','Zadar','13')";
-$result=mysqli_query($query);
+$query=" INSERT INTO mjesto(pbr,nazMjesto,sifZupanija)";
+$query=" VALUES ('23000','Zadar','13')";
+$result=mysqli_query($conn,$query);
 
 if(!$result)
     $error_num++;
 
 $query="INSERT INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES ('51000','Rijeka','8')";
-$result=mysqli_query($query);
+$query=" VALUES ('51000','Rijeka','8')";
+$result=mysqli_query($conn,$query);
 
 if(!$result)
     $error_num++;
 
 $query="INSERT INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES ('10000','Zagreb','21')";
-$result=mysqli_query($query);
+$query=" VALUES ('10000','Zagreb','21')";
+$result=mysqli_query($conn,$query);
 
 if(!$result)
     $error_num++;
 
-if($error_num<=0)
-    commit ();
-else 
+if($error_num <= 0){
+    commit();
+    echo "uspjesno";
+}
+else {
     rollback();
+    echo "greska ".$error_num;
+}
+
 ?>
+    
