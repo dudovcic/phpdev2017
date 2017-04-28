9d1e191083deba9354ef386038aa971d527efe69
NemanjaDragun92Git <nemanjadragun@gmail.com>
2017-04-28 21:17:23 +0200

    mysqli conn

---
+++ ndragun/PHP_and_MYSQL/dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "fakultet";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+
+?>
