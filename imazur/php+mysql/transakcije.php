a3dc666ead207598edb6b955c51361f2697254f3
ivanmazur001 <ivanmazur001@gmail.com>
2017-04-28 22:10:04 +0200

    predavanja-mysql poveziavnje

--- imazur/php+mysql/transakcije.php
+++ imazur/php+mysql/transakcije.php
@@ -1,25 +1,25 @@
 <?php
 
-include("db_connection.php");
+/*include("db_connection.php");
 
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('23000','Zadar',13)";
+$query.=" VALUES('23000','Zadar',13)";
 $result= mysqli_query($query);
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('51000','Rijeka',8)";
+$query.=" VALUES('51000','Rijeka',8)";
 $result= mysqli_query($query);
 
 $query="BEGIN";
 $result=mysqli_query($query);
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('23000','Zadar',13)";
+$query.=" VALUES('23000','Zadar',13)";
 $result= mysqli_query($query);
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('51000','Rijeka',8)";
+$query.=" VALUES('51000','Rijeka',8)";
 $result= mysqli_query($query);
 
 $query="COMMIT";
@@ -34,7 +34,7 @@ $query="BEGIN";
 $result=mysqli_query($query);
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('23000','Zadar',13)";
+$query.=" VALUES('23000','Zadar',13)";
 $result= mysqli_query($query);
 
 if(!$result)
@@ -53,46 +53,61 @@ else
 if(!$result)
     $error_num++;
 
-
-include("db_connection.php");
+*/
+include("dbconn.php");
 
 function begin()
-{$query="BEGIN";
-$result=mysqli_query($query);
+
+{
+    global $db;
+    $query="BEGIN";
+$result=mysqli_query($db,$query);
     
 }
 
 function commit()
-{$query="COMMIT";
-$result= mysqli_query($query);
+
+{global $db;
+    $query="COMMIT";
+$result= mysqli_query($db,$query);
 }
 
 function rollback()
-{$query="ROLLBACK";
-$result= mysqli_query($query);
+{global $db;
+    $query="ROLLBACK";
+$result= mysqli_query($db,$query);
 }
-
 begin();
 
 $error_num=0;
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('23000','Zadar',13)";
-$result= mysqli_query($query);
+$query.=" VALUES('23999','Zadar',13)";
+$result= mysqli_query($db,$query);
 
 if(!$result)
     $error_num++;
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('51000','Rijeka',8)";
-$result= mysqli_query($query);
+$query.=" VALUES('51999','Rijeka',8)";
+$result= mysqli_query($db,$query);
 
 if(!$result)
     $error_num++;
 
 $query="INSER INTO mjesto(pbr,nazMjesto,sifZupanija)";
-$query="VALUES('10000','Zagreb',21)";
-$result= mysqli_query($query);
+$query.=" VALUES('10999','Zagreb',21)";
+$result= mysqli_query($db,$query);
+
+if(!$result)
+    $error_num++;
+
+if($error_num<=0)
+    commit();
+else     
+    rollback();
+
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
