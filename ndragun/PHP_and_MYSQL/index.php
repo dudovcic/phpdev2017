59ab6aaac06027fd7e5503c15c638821d95bff2e
NemanjaDragun92Git <nemanjadragun@gmail.com>
2017-04-28 21:49:15 +0200

    Ispravljen query sa global

--- ndragun/PHP_and_MYSQL/index.php
+++ ndragun/PHP_and_MYSQL/index.php
@@ -11,16 +11,19 @@ include("dbconnect.php");
 
 <?php
 function begin(){
+    global $conn; 
     $query="BEGIN";
     $result= mysqli_query($conn, $query);
 }
 
 function commit(){
+    global $conn; 
     $query="COMMIT";
     $result = mysqli_query($conn,$query);
 }
 
 function rollback(){
+    global $conn; 
     $query="ROLLBACK";
     $result = mysqli_query($conn, $query);
 }
@@ -29,21 +32,21 @@ begin();
 
 $error_num = 0;
 
-$query="INESRT INTO mjesto(pbr,nazMjesto,sifZupanija)";
+$query="INSERT INTO mjesto1(pbr,nazMjesto,sifZupanija) ";
 $query.="VALUES ('23000', 'Zadara', 13)";
 $result = mysqli_query($conn, $query);
 
 if(!$result)
     $error_num++;
 
-$query="INSERT INTO mjestoo(pbr,nazMjesto,sifZupanija)";
+$query="INSERT INTO mjesto1(pbr,nazMjesto,sifZupanija) ";
 $query.="VALUES ('51000', 'Rijeka', '8')";
 $result = mysqli_query($conn, $query);
     
 if(!$result)
 $error_num++;
     
-$query="INSERT INTO mjestoo(pbr,nazMjesto,sifZupanija)";
+$query="INSERT INTO mjesto1(pbr,nazMjesto,sifZupanija) ";
 $query.="VALUES ('10000', 'Zagreb', '21')";
 $result = mysqli_query($conn, $query);
     
@@ -52,11 +55,14 @@ $error_num++;
 
 if($error_num <= 0){
     commit();
-echo 'uspjesno commit';}
-else{
+    echo '+';
+}else{
     rollback();
-    
+    echo '-';
 }
+
+    
+
 ?>
 
 </body>
