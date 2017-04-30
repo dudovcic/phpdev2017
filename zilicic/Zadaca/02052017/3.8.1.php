e76ee5fc98b1b546d6163b3c6e22f94cb2ccf76f
zilicic <zilicic5@h-1.hr>
2017-04-30 13:40:23 +0200

    Zadaca do zadatka3.8.3

---
+++ zilicic/Zadaca/02052017/3.8.1.php
@@ -0,0 +1,28 @@
+<?php
+
+//mysql_affected_rows
+
+include("db_connection.php");
+$query="SELECT * FROM stud WHERE pbrStan=10040";
+$result=mysqli_query($conn,$query); 
+if (!$result) {
+    die('Invalid query: ' . mysqli_error());
+}
+        echo 'Broj pronadjenih zapisa: '.mysqli_affected_rows($conn).'<br/>';
+
+        //drugi dio
+        
+$query="SELECT * FROM stud WHERE pbrStan=10040";
+$result=mysqli_query($conn,$query); 
+$br_result= mysqli_affected_rows($conn);
+if($br_result<=0)
+{
+    echo 'Ne postoji niti jedan rezultat';
+    
+}
+ else 
+     {
+while($student= mysqli_fetch_array($result))    
+{
+}
+}
---
+++ zilicic/Zadaca/02052017/3.8.2.php
@@ -0,0 +1,11 @@
+<?php
+
+//mysql_close
+
+include("db_connection.php");
+if(mysqli_close($conn))
+    echo 'Konekcija zatvorena';
+else 
+    echo 'Doslo je do greske';
+
+
