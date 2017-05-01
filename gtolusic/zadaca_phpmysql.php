9c51df5ae300b131f8e3b9103ef5e22a1fa3649f
gorant696 <axelfoley@net.hr>
2017-05-02 00:09:33 +0200

    drugi zadatak

---
+++ gtolusic/zadaca_phpmysql.php
@@ -0,0 +1,53 @@
+<?php
+
+$spajanje=mysqli_connect('localhost', 'root','');
+$baza=mysqli_select_db($spajanje, 'fakultet');
+mysqli_set_charset($spajanje,"utf8");
+
+if ($spajanje && $baza)
+    
+    echo "uspješno ste spojeni na database <br>";
+
+else
+    echo "ništa to";
+
+
+
+//prvi zadatak
+$query="select mbrstud, prezstud from stud where imestud='Ivan'";
+$result=mysqli_query($spajanje, $query);
+
+{
+    
+  echo "<table border=1><tr><th>Matićni broj</th><th>Prezime</th></tr>"
+    ;
+        while ($row=mysqli_fetch_assoc($result))
+       echo " <tr>
+        <td>".$row['mbrstud']."</td>
+        <td>".$row['prezstud']."</td>
+        </tr><br>";
+               
+}
+echo "</table>";
+
+
+
+//drugi zadatak
+
+$query2="select nazpred from pred order by nazpred";
+$result2=mysqli_query($spajanje, $query2);
+
+{
+    
+  echo "<table border=1><tr><th>Naziv predmeta</th></tr>"
+    ;
+        while ($row2=mysqli_fetch_assoc($result2))
+       echo " <tr>
+        <td>".$row2['nazpred']."</td>
+                </tr><br>";
+               
+}
+echo "</table>";
+
+//treci zadatak
+
