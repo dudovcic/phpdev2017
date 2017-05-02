1673cbd9ee8733bee9a8b9da55615a5e759b1163
curicdomagoj <curicdomagoj993@gmail.com>
2017-05-02 14:59:16 +0200

    num rows primjer

---
+++ dcuric/mysqltransakcije/primjer5.php
@@ -0,0 +1,28 @@
+<?php
+
+include ("db_connection.php");
+
+$query1="SELECt imeStud,prezStud";
+$query1 .= "FROM stud";
+$query1 .="WHERE pbrStan=10000";
+
+$result1= mysqli_query($conn,$query1);
+
+echo "Broj zapisu: ". mysqli_num_rows($result1);
+
+$br_result= mysqli_num_rows($result1);
+
+if($br_result<=0)
+{
+    echo "ne postoji ni jedan rezultata";
+}
+else
+{
+    while($student= mysqli_fetch_array($result1)){
+        echo "ispis";
+    }
+    }
+    
+?>
+
+    
