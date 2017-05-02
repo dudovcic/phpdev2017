1e1886dd8399c86335549252276c3d73aaaab036
gorant696 <axelfoley@net.hr>
2017-05-02 02:11:20 +0200

    dodane php-mysql funkcije iz knjige u zadatke

--- gtolusic/zadaca_phpmysql.php
+++ gtolusic/zadaca_phpmysql.php
@@ -14,7 +14,8 @@ die;}
 
 
 
-//prvi zadatak
+
+//prvi zadatak 3.10.1
 $query="select mbrstud, prezstud from stud where imestud='Ivan'";
 $result=mysqli_query($spajanje, $query);
 
@@ -30,10 +31,12 @@ $result=mysqli_query($spajanje, $query);
                
 }
 echo "</table>";
+echo "broj polja u zapisu:".mysqli_num_fields($result)."<br>";
+echo "broj pronadjenih zapisa:".mysqli_affected_rows($spajanje);
 
 
 
-//drugi zadatak
+//drugi zadatak 3.10.2
 
 $query2="select nazpred from pred order by nazpred";
 $result2=mysqli_query($spajanje, $query2);
@@ -49,8 +52,9 @@ $result2=mysqli_query($spajanje, $query2);
                
 }
 echo "</table>";
+echo "broj pronadjenih zapisa:".mysqli_affected_rows($spajanje);
 
-//treci zadatak
+//treci zadatak 3.10.3
 
 
 $query3="select imestud,prezstud from stud where prezstud like 'b%' order by imestud ASC";
@@ -68,8 +72,9 @@ $result3=mysqli_query($spajanje, $query3);
                
 }
 echo "</table>";
+echo "broj pronadjenih zapisa:".mysqli_affected_rows($spajanje);
 
-//cetvrti zadatak
+//cetvrti zadatak 3.10.4
 
 $query4="select imestud, prezstud from stud inner join mjesto on mjesto.pbr=stud.pbrrod where mjesto.nazmjesto='Zagreb'";
 $result4=mysqli_query($spajanje, $query4);
@@ -86,19 +91,27 @@ $result4=mysqli_query($spajanje, $query4);
                
 }
 echo "</table>";
+echo "broj pronadjenih zapisa:".mysqli_affected_rows($spajanje);
+echo "<br>";
 
-//peti zadatak
+//peti zadatak 3.10.5
 
 $query5="select imestud from stud where imestud like 'a%'";
 $result5=mysqli_query($spajanje, $query5);
 echo "broj redaka:".mysqli_num_rows($result5);
 echo "<br>";
 
-//šesti zadatak
+//šesti zadatak 3.10.6
 
 $query6="select count(preznastavnik) as brojnastavnika from nastavnik where preznastavnik like 'm%'";
 $result6=mysqli_query($spajanje, $query6);
 $broj=mysqli_fetch_assoc($result6);
 echo "broj prezimena koje počinju s m:".$broj['brojnastavnika'];
 
+echo "<br>";
+
+if (mysqli_close($spajanje))
+    echo "konekcija zatvorena";
+else
+    echo "doslo je do greske";
 
