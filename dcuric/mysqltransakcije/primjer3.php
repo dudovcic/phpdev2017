85865abd2231148cbca6276d1f9f94fa3d576aae
curicdomagoj <curicdomagoj993@gmail.com>
2017-05-02 19:18:24 +0200

    razvajanje query1

--- dcuric/mysqltransakcije/primjer3.php
+++ dcuric/mysqltransakcije/primjer3.php
@@ -3,7 +3,7 @@
 include ("db_connection.php");
 
 $query1="INSERT INTO dvorana (oznDvorana,kapacitet)";
-$query1.= "VALUES ('M2','14')";
+$query1.= " VALUES ('M2','14')";
 $result1= mysqli_query($conn, $query1);
 
 echo "zadnje pohranjeni ID: ".mysqli_insert_id($conn);
