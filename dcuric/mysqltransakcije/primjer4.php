3f7dd28e4870e949edec72d701ee0b0cd2e592ef
curicdomagoj <curicdomagoj993@gmail.com>
2017-05-02 14:48:17 +0200

    primjer1 zadaca num-greska11 linija

---
+++ dcuric/mysqltransakcije/primjer4.php
@@ -0,0 +1,13 @@
+<?php
+
+include ("db_connection.php");
+
+$query1="SELECt imeStud,prezStud";
+$query1 .= "FROM stud";
+$query1 .="WHERE pbrStan=10040";
+
+$result1= mysqli_query($conn,$query1);
+
+echo "Broj polja u zapisu: ".mysqli_num_fields($result1);
+
+?>
